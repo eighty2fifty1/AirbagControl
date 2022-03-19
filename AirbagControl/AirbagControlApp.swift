bd71367cf55be1c0ee2b15e9ee5bb3b00e72c4a7
James Ford <huskernavy70@gmail.com>
2022-03-19 10:18:17 -0700

    Initial Commit

---
+++ AirbagControl/AirbagControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbagControlApp.swift
+//  AirbagControl
+//
+//  Created by James Ford on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AirbagControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
